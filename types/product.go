b2797053f662db986b61cee4563d14acb4694778
Miha <luksic.miha@gmail.com>
2023-08-03 22:19:42 +0200

    feat: create new types

--- types/product.go
+++ types/product.go
@@ -5,3 +5,25 @@ type Product struct {
 	Price int
 	Url   string
 }
+
+func NewProduct(name string, price int, url string) *Product {
+	return &Product{
+		Name:  name,
+		Price: price,
+		Url:   url,
+	}
+}
+
+type ProductPrice struct {
+	Name      string
+	ProductId int
+	Price     int
+}
+
+func NewProductPrice(name string, productId int, price int) *ProductPrice {
+	return &ProductPrice{
+		Name:      name,
+		ProductId: productId,
+		Price:     price,
+	}
+}

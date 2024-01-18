e3763c6aced512cff469931b0e0219e37ae92e25
Miha <luksic.miha@gmail.com>
2024-01-18 01:39:09 +0100

    feat: run migrations on startup

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.21
 require (
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/gocolly/colly v1.2.0
-	github.com/jackc/pgx/v5 v5.4.3
+	github.com/jackc/pgx/v5 v5.5.1
+	github.com/pressly/goose/v3 v3.17.0
+	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.16.0
 )
 
@@ -23,24 +25,28 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
-	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240116215550-a9fa1716bcac // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- storage/postgresdb.go
+++ storage/postgresdb.go
@@ -1,34 +1,46 @@
 package storage
 
 import (
-	"context"
+	"database/sql"
+	"embed"
 	"fmt"
-	"github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/mluksic/product-price-tracker/types"
+	"github.com/pressly/goose/v3"
 	"github.com/spf13/viper"
 	"os"
 )
 
 type PostgresStorage struct {
-	db *pgx.Conn
+	db *sql.DB
 }
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
 
 func NewPostgresStorage() *PostgresStorage {
 	dbUrl := fmt.Sprintf("%s", viper.Get("DB_URL"))
-	conn, err := pgx.Connect(context.Background(), dbUrl)
+	db, err := sql.Open("pgx", dbUrl)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 	//defer conn.Close(context.Background())
+	goose.SetBaseFS(embedMigrations)
+	if err := goose.SetDialect("postgres"); err != nil {
+		panic(err)
+	}
+	if err := goose.Up(db, "migrations"); err != nil {
+		panic(err)
+	}
 
 	return &PostgresStorage{
-		db: conn,
+		db: db,
 	}
 }
 
 func (p PostgresStorage) GetProductPrices(pId int) ([]types.ProductPrice, error) {
-	rows, err := p.db.Query(context.Background(), "select pp.price, pp.fetched_at from product_price pp where product_id = $1", pId)
+	rows, err := p.db.Query("select pp.price, pp.fetched_at from product_price pp where product_id = $1", pId)
 	if err != nil {
 		return nil, err
 	}
@@ -49,25 +61,25 @@ func (p PostgresStorage) GetProductPrices(pId int) ([]types.ProductPrice, error)
 }
 
 func (p PostgresStorage) CreateProductPrice(productPrice *types.ProductPrice) error {
-	_, err := p.db.Exec(context.Background(), "insert into product_price(price, fetched_at, product_id) values ($1, $2, $3)", productPrice.Price, productPrice.FetchedAt, productPrice.ProductId)
+	_, err := p.db.Exec("insert into product_price(price, fetched_at, product_id) values ($1, $2, $3)", productPrice.Price, productPrice.FetchedAt, productPrice.ProductId)
 
 	return err
 }
 
 func (p PostgresStorage) CreateProduct(product *types.Product) error {
-	_, err := p.db.Exec(context.Background(), "insert into product (name, is_tracked, url, created_at, updated_at) values ($1,$2,$3,$4,$5)", product.Name, product.IsTracked, product.Url, product.CreatedAt, product.UpdatedAt)
+	_, err := p.db.Exec("insert into product (name, is_tracked, url, created_at, updated_at) values ($1,$2,$3,$4,$5)", product.Name, product.IsTracked, product.Url, product.CreatedAt, product.UpdatedAt)
 
 	return err
 }
 
 func (p PostgresStorage) DeleteProduct(id int) error {
-	_, err := p.db.Exec(context.Background(), "delete from product where id = $1", id)
+	_, err := p.db.Exec("delete from product where id = $1", id)
 
 	return err
 }
 
 func (p PostgresStorage) GetProducts() ([]types.Product, error) {
-	rows, err := p.db.Query(context.Background(), "select id, name, is_tracked, url, created_at, updated_at from product order by id desc")
+	rows, err := p.db.Query("select id, name, is_tracked, url, created_at, updated_at from product order by id desc")
 
 	if err != nil {
 		return nil, err
@@ -87,7 +99,7 @@ func (p PostgresStorage) GetProducts() ([]types.Product, error) {
 }
 
 func (p PostgresStorage) GetProduct(id int) (types.Product, error) {
-	row, err := p.db.Query(context.Background(), "select id, name, is_tracked, url, created_at, updated_at from product where id = $1", id)
+	row, err := p.db.Query("select id, name, is_tracked, url, created_at, updated_at from product where id = $1", id)
 	if err != nil {
 		return types.Product{}, err
 	}
@@ -105,7 +117,7 @@ func (p PostgresStorage) GetProduct(id int) (types.Product, error) {
 }
 
 func (p PostgresStorage) ToggleProductTracking(id int) error {
-	_, err := p.db.Exec(context.Background(), "update product set is_tracked = not is_tracked where id = $1", id)
+	_, err := p.db.Exec("update product set is_tracked = not is_tracked where id = $1", id)
 
 	return err
 }

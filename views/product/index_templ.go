faa3ba5a53475b43a1cbe2587c5962880a2b6f03
Miha <luksic.miha@gmail.com>
2024-03-05 22:26:42 +0100

    bug: change PUT to PORT for manual scrape

--- views/product/index_templ.go
+++ views/product/index_templ.go
@@ -323,7 +323,7 @@ func actionButtons(product types.Product) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</button> <button hx-put=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</button> <button hx-post=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
